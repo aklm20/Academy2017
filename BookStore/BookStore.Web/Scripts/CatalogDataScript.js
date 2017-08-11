21e024058917b61cff9dbcce337f7fed54197f4e
Karen <anakalo94@gmail.com>
2017-08-11 14:40:42 -0700

    mvc

---
+++ BookStore/BookStore.Web/Scripts/CatalogDataScript.js
@@ -0,0 +1,9 @@
+﻿var MyCatalogData = {
+    "CinderBook":
+    {
+        "id": 0,
+        "title": "CinderTitle",
+        "description": "CinderDescription",
+        "imgUrl": "Images/Cinder_cover.jpg"
+    }
+}
---
+++ BookStore/BookStore.Web/Scripts/CatalogScript.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $("#DynamicHTML").append(buildMyProduct(MyCatalogData));
+});
+
+//Dynamic build your collection
+function buildMyProduct(obj) {
+    var html = "";
+    $.each(obj, function (element, object) {
+        html += "<div class='Catalog ProducItem'>";
+        html += "<div><a href='details.html?id=" + object.id + "'>" + object.title + "</a></div>";
+        html += "<div><img class='Catalog' src='" + object.imgUrl + "' /></div>";
+        html += "<div>" + object.description + "</div></div>";
+    });
+    return html;
+}
